9a8cf375598b4abbd429db9825da1c21269136a0
Spzabt-zz <stoida146@gmail.com>
2021-02-24 22:09:28 +0200

    firstCommit

---
+++ Lab1_Sem2_Var7/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Lab1_Sem2_Var7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button1.Location = new System.Drawing.Point(12, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label1.Location = new System.Drawing.Point(40, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hide Me";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label3.Location = new System.Drawing.Point(392, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hide Me";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label4.Location = new System.Drawing.Point(564, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Hide Me";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button2.Location = new System.Drawing.Point(185, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 100);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Hide";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button3.Location = new System.Drawing.Point(358, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 100);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Hide";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button4.Location = new System.Drawing.Point(531, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 100);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Hide";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label2.Location = new System.Drawing.Point(218, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Hide Me";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button5.Location = new System.Drawing.Point(531, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(167, 57);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Reload game";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Lab1_Sem2_Var7.Properties.Resources.unnamed;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Azino777";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button5;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+    }
+}
---
+++ Lab1_Sem2_Var7/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_Sem2_Var7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetSettings()
+        {
+            Random rand = new Random();
+
+            /*label1.Text = rand.Next(10).ToString();
+            label2.Text = rand.Next(10).ToString();
+            label3.Text = rand.Next(10).ToString();
+            label4.Text = rand.Next(10).ToString();*/
+
+            var firstColorSet = label1.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+            var secondColorSet = label2.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+            var thirdColorSet = label3.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+            var fourthColorSet = label4.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+
+            label1.ForeColor = firstColorSet;
+            label2.ForeColor = secondColorSet;
+            label3.ForeColor = thirdColorSet;
+            label4.ForeColor = fourthColorSet;
+        }
+
+        private void SetButtonColor(Button b)
+        {
+            Random rand = new Random();
+
+            b.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetSettings();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button1);
+
+            label1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button2);
+
+            label1.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SetSettings();
+        }
+    }
+}
