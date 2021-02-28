9d304baf3f4b45c3d3cc3e4813916ef25435ca51
Spzabt-zz <stoida146@gmail.com>
2021-03-01 01:18:11 +0200

    task4 com4

---
+++ AdditionalTask4/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AdditionalTask4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InformationalLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.InformationalLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(704, 441);
+            this.splitContainer1.SplitterDistance = 91;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Information";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InformationalLabel
+            // 
+            this.InformationalLabel.Location = new System.Drawing.Point(0, 407);
+            this.InformationalLabel.Name = "InformationalLabel";
+            this.InformationalLabel.Size = new System.Drawing.Size(87, 23);
+            this.InformationalLabel.TabIndex = 0;
+            this.InformationalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 375);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create label";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label InformationalLabel;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+
+        #endregion
+    }
+}
---
+++ AdditionalTask4/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using ContentAlignment = System.Drawing.ContentAlignment;
+
+namespace AdditionalTask4
+{
+    public partial class Form1 : Form
+    {
+        private readonly VScrollBar _scrollBar = new VScrollBar();
+        private int _iter = 1;
+        private const int X = 3;
+        private int _y = 7; // down += 23
+
+        delegate void OnClick(string mess);
+        private event OnClick Click;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            _scrollBar.Dock = DockStyle.Right;
+            _scrollBar.Height = splitContainer1.Panel2.Height;
+            _scrollBar.Width = 20;
+            
+            splitContainer1.Panel2.AutoScroll = false;
+            splitContainer1.Panel2.VerticalScroll.Enabled = false;
+            splitContainer1.Panel2.VerticalScroll.Visible = false;
+            splitContainer1.Panel2.VerticalScroll.Maximum = 0;
+            
+            splitContainer1.Panel2.HorizontalScroll.Enabled = false;
+            splitContainer1.Panel2.HorizontalScroll.Visible = false;
+            splitContainer1.Panel2.HorizontalScroll.Maximum = 0;
+            //splitContainer1.Panel2.AutoScroll = true;
+
+            _scrollBar.Minimum = 0;
+            _scrollBar.Value = 0;
+            _scrollBar.Maximum = 1000;
+            
+            splitContainer1.Panel2.VerticalScroll.Maximum = _scrollBar.Maximum;
+
+            _scrollBar.Scroll += scrollBar_Scroll;
+            
+            Controls.Add(_scrollBar);
+        }
+
+        private void ShowInfo(string mess)
+        {
+            //InformationalLabel.Text
+        }
+
+        private void scrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            splitContainer1.Panel2.VerticalScroll.Value = _scrollBar.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label label = new Label();
+            label.Location = new Point(X, _y);
+            label.TextAlign = ContentAlignment.TopLeft;
+            label.Text = $"label{_iter}";
+            var fontstyle = FontStyle.Bold | FontStyle.Italic;
+            label.Font = new Font("Arial", 10f, fontstyle);
+            splitContainer1.Panel2.Controls.Add(label);
+            _iter++;
+            _y += 23;
+        }
+    }
+}
