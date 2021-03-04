4916b350877da816a74f28fb5a0564667cfb024f
Szpabt-zz <stoida146@gmail.com>
2021-03-04 23:50:36 +0200

    SharpWasher commit

---
+++ Lab1_OOP_Sem2/Lab1_Sem2_Var7/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_Sem2_Var7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetSettings()
+        {
+            Random rand = new Random();
+
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+
+            var firstColorSet = label1.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+            var secondColorSet = label2.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+            var thirdColorSet = label3.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+            var fourthColorSet = label4.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+
+            label1.ForeColor = firstColorSet;
+            label2.ForeColor = secondColorSet;
+            label3.ForeColor = thirdColorSet;
+            label4.ForeColor = fourthColorSet;
+        }
+
+        private void SetButtonColor(Button b)
+        {
+            Random rand = new Random();
+
+            b.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256),
+                rand.Next(256));
+        }
+
+        private void Calculate(Label l1, Label l2)
+        {
+            Random random = new Random();
+
+            switch (random.Next(4))
+            {
+                case 0:
+                    l1.Visible = false;
+                    break;
+                case 1:
+                    l1.Visible = true;
+                    break;
+                case 2:
+                    l1.Visible = false;
+                    l2.Visible = true;
+                    break;
+                case 3:
+                    l1.Visible = true;
+                    l2.Visible = false;
+                    break;
+                default:
+                    MessageBox.Show("???");
+                    break;
+            }
+
+            if (label1.Visible == false && label2.Visible == false
+                                        && label3.Visible == false && label4.Visible == false)
+                MessageBox.Show("You won, press restart button or exit!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetSettings();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button1);
+            Calculate(label3, label2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button2);
+            Calculate(label4, label1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button3);
+            Calculate(label2, label4);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SetButtonColor(button4);
+            Calculate(label1, label3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SetSettings();
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Lab1_OOP_Sem2/SecondTask/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace SecondTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label3.Location = new System.Drawing.Point(12, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button1.Location = new System.Drawing.Point(12, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Submit personal data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 164);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 24);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Cleaning";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.checkBox2.Location = new System.Drawing.Point(12, 194);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(118, 24);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Cavity";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.checkBox3.Location = new System.Drawing.Point(12, 224);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(118, 24);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Filling";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.checkBox4.Location = new System.Drawing.Point(12, 254);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(118, 24);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "X-Ray";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.checkBox5.Location = new System.Drawing.Point(12, 284);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(118, 24);
+            this.checkBox5.TabIndex = 11;
+            this.checkBox5.Text = "Fluoride";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label4.Location = new System.Drawing.Point(12, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 23);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Choose service";
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.checkBox6.Location = new System.Drawing.Point(12, 314);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(118, 24);
+            this.checkBox6.TabIndex = 14;
+            this.checkBox6.Text = "Root Canal";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button2.Location = new System.Drawing.Point(12, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 28);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(136, 352);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 14);
+            this.label5.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(136, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 14);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "54$";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(136, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 14);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "345$";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(136, 230);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 14);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "123$";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(136, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 14);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "71$";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(136, 290);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 14);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "500$";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(136, 320);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 14);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "66$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 383);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.CheckBox checkBox6;
+
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.CheckBox checkBox1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
